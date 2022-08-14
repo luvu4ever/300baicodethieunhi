cf3e70b8a46edce4b6f0d0e025b152039cdfe734
luvu4ever <dungfixbug@gmail.com>
2022-08-14 12:36:10 +0700

    twosum

---
+++ 1. twosum/twosum.c
@@ -0,0 +1,15 @@
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    int *ans = malloc(2*sizeof(int));
+    for(int i=0; i<numsSize; i++){
+        for(int j=i+1; j<numsSize; j++){
+            if(nums[i] + nums[j] == target){
+                *returnSize = 2;
+                ans[0] = i;
+                ans[1] = j;
+                return ans;
+            }
+        }
+    }
+    *returnSize = 0;
+    return NULL;
+}
